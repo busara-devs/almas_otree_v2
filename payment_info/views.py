7df0a98c640da2d6d59403315c62265dcefe287a
jobm <jobappdeveloper@outlook.com>
2017-09-07 12:03:45 +0300

    getting payoff of 3 random games

--- payment_info/views.py
+++ payment_info/views.py
@@ -4,21 +4,28 @@ from . import models
 from ._builtin import Page, WaitPage
 from otree.common import Currency as c, currency_range
 from .models import Constants
-import math
+import random
 
 
 class PaymentInfo(Page):
     def is_displayed(self):
-        self.player.payoff = self.player.participant.vars["carrying_payoff"]
+        game_obj = self.participant.vars["game_payoff"].keys()
+        self.player.total_payoff = sum(payoffs(game_obj))
         return True
 
     def vars_for_template(self):
         participant = self.player.participant
         return {
             'redemption_code': participant.label or participant.code,
-            'payoff': math.floor(participant.vars["carrying_payoff"]),
-            'apoints': participant.vars["vouchers"]
+            'payoff': self.player.total_payoff,
+            'vouchers': participant.vars["vouchers"]
         }
+
+def payoffs(game_obj):
+    games = game_obj.keys()
+    games = random.sample(games, 3)
+    for game in games:
+        yield game_obj[game]
 
 
 page_sequence = [PaymentInfo]

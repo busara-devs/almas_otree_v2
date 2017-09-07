ecfd566ba866a3fffb98537e445918d441cbb6f8
jobm <jobappdeveloper@outlook.com>
2017-09-07 11:36:28 +0300

    changed tokens to KSH in TP

--- time_preference/models.py
+++ time_preference/models.py
@@ -40,7 +40,6 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-
     def set_choices(self):
         choices = []
         menu_a = ["menu_a_q1", "menu_a_q2", "menu_a_q3", "menu_a_q4", "menu_a_q5", "menu_a_q6"]
@@ -76,34 +75,33 @@ class Player(BasePlayer):
         self.participant.vars["game_payoff"]["time_preference"] = points
         self.time_preference_points = points
 
+    q1_a = (('240_now', 'A: KSH 240'), ('0_future', 'B: KSH 0'),)
+    q2_a = (('192_now', 'A: ksh 192 '), ('70_future', 'B: KSH 70'),)
+    q3_a = (('144_now', 'A: KSH 144 '), ('140_future', 'B: KSH 140'),)
+    q4_a = (('96_now', 'A: KSH 96 '), ('210_future', 'B: KSH 210'),)
+    q5_a = (('48_now', 'A: KSH 48 '), ('275_future', 'B: KSH 275'),)
+    q6_a = (('0_now', 'A: ksh 0'), ('340_future', 'B: KSH 340'),)
 
-    q1_a = (('840_now', 'A: 840 Tokens'), ('0_future', 'B: 0 Tokens'),)
-    q2_a = (('672_now', 'A: 672 Tokens'), ('240_future', 'B: 240 Tokes'),)
-    q3_a = (('504_now', 'A: 504 Tokens'), ('480_future', 'B: 480 Tokens'),)
-    q4_a = (('336_now', 'A: 336 Tokens'), ('720_future', 'B: 720 Tokens'),)
-    q5_a = (('168_now', 'A: 168 Tokens'), ('960_future', 'B: 960 Tokens'),)
-    q6_a = (('0_now', 'A: 0 Tokens'), ('1200_future', 'B: 1200 Tokens'),)
+    q1_b = (('290_now', 'A: KSH 290'), ('0_future', 'B: KSH 0'),)
+    q2_b = (('250_now', 'A: KSH 250'), ('53_future', 'B: KSH 53'),)
+    q3_b = (('180_now', 'A: KSH 180'), ('110_future', 'B: KSH 110'),)
+    q4_b = (('120_now', 'A: KSH 120'), ('170_future', 'B: KSH 170'),)
+    q5_b = (('64_now', 'A: KSH 64'), ('230_future', 'B: KSH 230'),)
+    q6_b = (('0_now', 'A: KSH 0'), ('290_future', 'B: KSH 290'),)
 
-    q1_b = (('1020_now', 'A: 1020 Tokens'), ('0_future', 'B: 0 Tokens'),)
-    q2_b = (('836_now', 'A: 836 Tokens'), ('184_future', 'B: 184 Tokens'),)
-    q3_b = (('632_now', 'A: 632 Tokens'), ('388_future', 'B: 388 Tokens'),)
-    q4_b = (('428_now', 'A: 428 Tokens'), ('592_future', 'B: 592 Tokens'),)
-    q5_b = (('224_now', 'A: 224 Tokens'), ('796_future', 'B: 796 Tokens'),)
-    q6_b = (('0_now', 'A: 0 Tokens'), ('1020_future', 'B: 1020 Tokens'),)
+    q1_c = (('240_now', 'A: KSH 240'), ('0_future', 'B: KSH 0'),)
+    q2_c = (('192_now', 'A: KSH 192'), ('70_future', 'B: KSH 70'),)
+    q3_c = (('144_now', 'A: KSH 144 '), ('140_future', 'B: KSH 140'),)
+    q4_c = (('96_now', 'A: KSH 96'), ('210_future', 'B: KSH 210'),)
+    q5_c = (('48_now', 'A: KSH 48 '), ('960_future', 'B: KSH 275'),)
+    q6_c = (('0_now', 'A: KSH 0'), ('1200_future', 'B: KSH 340'),)
 
-    q1_c = (('840_now', 'A: 840 Tokens'), ('0_future', 'B: 0 Tokens'),)
-    q2_c = (('672_now', 'A: 672 Tokens'), ('240_future', 'B: 240 Tokes'),)
-    q3_c = (('504_now', 'A: 504 Tokens'), ('480_future', 'B: 480 Tokens'),)
-    q4_c = (('336_now', 'A: 336 Tokens'), ('720_future', 'B: 720 Tokens'),)
-    q5_c = (('168_now', 'A: 168 Tokens'), ('960_future', 'B: 960 Tokens'),)
-    q6_c = (('0_now', 'A: 0 Tokens'), ('1200_future', 'B: 1200 Tokens'),)
-
-    q1_d = (('1020_now', 'A: 1020 Tokens'), ('0_future', 'B: 0 Tokens'),)
-    q2_d = (('836_now', 'A: 836 Tokens'), ('184_future', 'B: 184 Tokens'),)
-    q3_d = (('632_now', 'A: 632 Tokens'), ('388_future', 'B: 388 Tokens'),)
-    q4_d = (('428_now', 'A: 428 Tokens'), ('592_future', 'B: 592 Tokens'),)
-    q5_d = (('224_now', 'A: 224 Tokens'), ('796_future', 'B: 796 Tokens'),)
-    q6_d = (('0_now', 'A: 0 Tokens'), ('1020_future', 'B: 1020 Tokens'),)
+    q1_d = (('290_now', 'A: KSH 290'), ('0_future', 'B: KSH 0'),)
+    q2_d = (('240_now', 'A: KSH 240'), ('50_future', 'B: KSH 50'),)
+    q3_d = (('180_now', 'A: KSH 180'), ('110_future', 'B: KSH 110'),)
+    q4_d = (('120_now', 'A: kSH 120'), ('170_future', 'B: KSH 170'),)
+    q5_d = (('64_now', 'A: KSH 64 '), ('230_future', 'B: KSH 230'),)
+    q6_d = (('0_now', 'A: KSH 0'), ('290_future', 'B: KSH 290'),)
 
     menu_a_q1 = models.CharField(choices=q1_a, verbose_name="", widget=widgets.RadioSelect())
     menu_a_q2 = models.CharField(choices=q2_a, verbose_name="", widget=widgets.RadioSelect())

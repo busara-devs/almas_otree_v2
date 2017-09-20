75982effb7c879b5a4b3eb2299a5b499c5dd5ead
jobm <jobappdeveloper@outlook.com>
2017-09-20 12:39:39 +0300

    corrected airtime voucher in payment info

--- payment_info/views.py
+++ payment_info/views.py
@@ -30,7 +30,7 @@ class PaymentInfo(Page):
                 'payoff': "{0:.2f}".format(participant.vars.get("carrying_payoff", None)),
                 'payoff_usd': c(participant.vars.get("carrying_payoff", None)).to_real_world_currency(self.session),
                 'vouchers': participant.vars.get("vouchers", None),
-                'airtime_worth': 1 * participant.vars.get("vouchers", None),
+                'airtime_worth': 50 * participant.vars.get("vouchers", None),
 
                 'menu_a_b_today': "{0:.2f}".format(participant.vars.get("menu_a_b_today", None)["today"]),
                 'menu_a_b_today_usd': c(participant.vars.get("menu_a_b_today", None)["today"]).to_real_world_currency(
@@ -47,7 +47,7 @@ class PaymentInfo(Page):
                 'payoff': "{0:.2f}".format(participant.vars.get("carrying_payoff", None)),
                 'payoff_usd': c(participant.vars.get("carrying_payoff", None)).to_real_world_currency(self.session),
                 'vouchers': participant.vars.get("vouchers", None),
-                'airtime_worth': 1 * participant.vars.get("vouchers", None),
+                'airtime_worth': 50 * participant.vars.get("vouchers", None),
 
                 'menu_c_d_3weeks': "{0:.2f}".format(participant.vars.get("menu_c_d_3weeks", None)["weeks3"]),
                 'menu_c_d_3weeks_usd': c(
@@ -63,7 +63,7 @@ class PaymentInfo(Page):
                 'payoff': "{0:.2f}".format(participant.vars.get("carrying_payoff", None)),
                 'payoff_usd': c(participant.vars.get("carrying_payoff", None)).to_real_world_currency(self.session),
                 'vouchers': participant.vars.get("vouchers", None),
-                'airtime_worth': 1 * participant.vars.get("vouchers", None),
+                'airtime_worth': 50 * participant.vars.get("vouchers", None),
             }
 
 

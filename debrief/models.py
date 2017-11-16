01836b5c0e7e47c15843b2fd76419611b9fc13c3
jobm <jobappdeveloper@outlook.com>
2017-11-16 08:21:49 +0300

    added debrief survey

--- debrief/models.py
+++ debrief/models.py
@@ -1,8 +1,10 @@
+from django.forms import forms, widgets as dj_widgets
+from django import forms
 from otree.api import (
     models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
     Currency as c, currency_range
 )
-
+from django.db import models as dj_models
 
 author = 'Your name here'
 
@@ -15,6 +17,13 @@ class Constants(BaseConstants):
     name_in_url = 'debrief'
     players_per_group = None
     num_rounds = 1
+    CHOICES_Q9 = [('NA', ''),
+                  ('Not very confident', ''),
+                  ('Somewhat confident', ''),
+                  ('Moderately confident', ''),
+                  ('Very confident', ''),
+                  ('Prefer not to answer', ''),
+                  ]
 
 
 class Subsession(BaseSubsession):
@@ -26,4 +35,75 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    pass
+    q1 = models.CharField(choices=['Comfortable', 'Uncomfortable'], widget=widgets.RadioSelect())
+    q1_comment = models.CharField(widget=forms.Textarea(
+        attrs={"placeholder": "Please share any other comments on chair comfort:",
+               "style": "height: 100px; width: 70%; padding: 8px;"}
+    ), blank=True)
+
+    q2 = models.CharField(choices=['Comfortable', 'Too bright', 'Too dim'], widget=widgets.RadioSelect())
+    q2_comment = models.CharField(widget=forms.Textarea(
+        attrs={"placeholder": "Please share any other comments on screen brightness:",
+               "style": "height: 100px; width: 70%; padding: 8px;"}
+    ), blank=True)
+
+    q3 = models.CharField(choices=['Comfortable', 'Too dry', 'Too humid'], widget=widgets.RadioSelect())
+    q3_comment = models.CharField(widget=forms.Textarea(
+        attrs={"placeholder": "Please share any other comments on air quality:",
+               "style": "height: 100px; width: 70%; padding: 8px;"}
+    ), blank=True)
+
+    q4 = models.CharField(choices=['Comfortable', 'Too cold', 'Too hot'], widget=widgets.RadioSelect())
+    q4_comment = models.CharField(widget=forms.Textarea(
+        attrs={"placeholder": "Please share any other comments on room temperature:",
+               "style": "height: 100px; width: 70%; padding: 8px;"}
+    ), blank=True)
+
+    q5 = models.CharField(choices=['Comfortable', 'Too bright', 'Too dim'], widget=widgets.RadioSelect())
+    q5_comment = models.CharField(widget=forms.Textarea(
+        attrs={"placeholder": "Please share any other comments on room lighting:",
+               "style": "height: 100px; width: 70%; padding: 8px;"}
+    ), blank=True)
+
+    q6 = models.CharField(choices=['Comfortable', 'Too empty', 'Too crowded'], widget=widgets.RadioSelect())
+    q6_comment = models.CharField(widget=forms.Textarea(
+        attrs={"placeholder": "Please share any other comments on room spacing:",
+               "style": "height: 100px; width: 70%; padding: 8px;"}
+    ), blank=True)
+
+    q7 = models.CharField(choices=['Comfortable', 'Too silent', 'Too noisy'], widget=widgets.RadioSelect())
+    q7_comment = models.CharField(widget=forms.Textarea(
+        attrs={"placeholder": "Please share any other comments on noise level:",
+               "style": "height: 100px; width: 70%; padding: 8px;"}
+    ), blank=True)
+
+    q8_g = models.CharField(choices=[('Generosity', 'Generosity')],widget=widgets.RadioSelect(), blank=True)
+    q8_sb = models.CharField(choices=[('Screen brightness', 'Screen brightness')], widget=widgets.RadioSelect(), blank=True)
+    q8_rl = models.CharField(choices=[('Room lighting', 'Room lighting')], widget=widgets.RadioSelect(), blank=True)
+    q8_gd = models.CharField(choices=[('Gender Discrimination', 'Gender Discrimination')], widget=widgets.RadioSelect(), blank=True)
+    q8_rt = models.CharField(choices=[('Room temperature', 'Room temperature')], widget=widgets.RadioSelect(), blank=True)
+    q8_rs = models.CharField(choices=[('Room space', 'Room space')], widget=widgets.RadioSelect(), blank=True)
+    q8_an = models.CharField(choices=[('Ambient noise', 'Ambient noise')], widget=widgets.RadioSelect(), blank=True)
+    q8_none = models.CharField(choices=[('None of the above', 'None of the above')], widget=widgets.RadioSelect(), blank=True)
+    q8_other_c = models.CharField(choices=[('other', 'other')], widget=widgets.RadioSelect(), blank=True)
+
+    q8_other = models.CharField(widget=forms.Textarea(
+        attrs={"style": "height: 100px; width: 70%; padding: 8px;", "id": "q8_other", "placeholder": "Other:"}
+    ), blank=True)
+
+    q9_g = models.CharField(choices=Constants.CHOICES_Q9, widget=widgets.RadioSelect(), blank=True)
+    q9_sb = models.CharField(choices=Constants.CHOICES_Q9, widget=widgets.RadioSelect(), blank=True)
+    q9_rl = models.CharField(choices=Constants.CHOICES_Q9, widget=widgets.RadioSelect(), blank=True)
+    q9_gd = models.CharField(choices=Constants.CHOICES_Q9, widget=widgets.RadioSelect(), blank=True)
+    q9_rt = models.CharField(choices=Constants.CHOICES_Q9, widget=widgets.RadioSelect(), blank=True)
+    q9_rs = models.CharField(choices=Constants.CHOICES_Q9, widget=widgets.RadioSelect(), blank=True)
+    q9_an = models.CharField(choices=Constants.CHOICES_Q9, widget=widgets.RadioSelect(), blank=True)
+    q9_other = models.CharField(choices=Constants.CHOICES_Q9, widget=widgets.RadioSelect(), blank=True)
+
+    q10 = models.CharField(choices=['Upon entering the room',
+                                    'While waiting for the experiment to begin',
+                                    'Sometime during the experiment',
+                                    'During the computer survey',
+                                    'During this paper survey',
+                                    'Prefer not to answer',
+                                    'NA'], widget=widgets.RadioSelect())
--- debrief/views.py
+++ debrief/views.py
@@ -4,22 +4,27 @@ from ._builtin import Page, WaitPage
 from .models import Constants
 
 
-class MyPage(Page):
-    pass
-
-
-class ResultsWaitPage(WaitPage):
-
-    def after_all_players_arrive(self):
-        pass
-
-
-class Results(Page):
-    pass
+class Debrief(Page):
+    form_fields = ["q1", "q1_comment",
+                   "q2", "q2_comment",
+                   "q3", "q3_comment",
+                   "q4", "q4_comment",
+                   "q5", "q5_comment",
+                   "q6", "q6_comment",
+                   "q7", "q7_comment",
+                   "q8_g", "q8_sb",
+                   "q8_rl", "q8_gd",
+                   "q8_rt", "q8_rs",
+                   "q8_an", "q8_none",
+                   "q8_other", "q8_other_c",
+                   "q9_g", "q9_sb",
+                   "q9_rl", "q9_gd",
+                   "q9_rt", "q9_rs",
+                   "q9_an", "q9_other",
+                   "q10"]
+    form_model = models.Player
 
 
 page_sequence = [
-    MyPage,
-    ResultsWaitPage,
-    Results
+    Debrief
 ]
